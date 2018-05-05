243c5e662f46ebfe765e920a2ecd02ab4736ea00
Wozniaxos <kacperwozniax@gmail.com>
2018-05-05 22:16:06 +0200

    Created factory for all initial data needed.

--- soundbuddy/models.py
+++ soundbuddy/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Instrument(models.Model):
-    name = models.CharField(max_length=30, null=True)
-    type = models.CharField(max_length=30, null=True)
+    name = models.CharField(max_length=100, null=True)
+    type = models.CharField(max_length=100, null=True)
     def __str__(self):
         return self.name
 
 class EventKind(models.Model):
-    name = models.CharField(max_length=30, null=True)
+    name = models.CharField(max_length=100, null=True)
     def __str__(self):
         return self.name
 
 class Event(models.Model):
-    type = models.CharField(max_length=30, null=True)
+    type = models.CharField(max_length=100, null=True)
     address = models.CharField(max_length=100, null=True)
     date = models.DateField(null=True)
     kind = models.ForeignKey(EventKind, on_delete=models.CASCADE, null=True)
@@ -23,13 +23,13 @@ class Event(models.Model):
         return self.kind.name
 
 class MusicType(models.Model):
-    name = models.CharField(max_length=30, null=True)
+    name = models.CharField(max_length=100, null=True)
     def __str__(self):
         return self.name
 
 class Band(models.Model):
-    name = models.CharField(max_length = 30, null=True)
-    city = models.CharField(max_length = 30, null=True)
+    name = models.CharField(max_length = 100, null=True)
+    city = models.CharField(max_length = 100, null=True)
     available_over_sea = models.BooleanField(default = False)
     willing_play_in_another_configuration = models.BooleanField(default = False)
     perform_radius = models.IntegerField(null=True)
@@ -42,33 +42,33 @@ class Band(models.Model):
         return self.name
 
 class Photo(models.Model):
-    url = models.CharField(max_length=30, null=True)
+    url = models.CharField(max_length=100, null=True)
     band = models.ForeignKey(Band, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.url
 
 class Track(models.Model):
-    url = models.CharField(max_length=30, null=True)
+    url = models.CharField(max_length=100, null=True)
     band = models.ForeignKey(Band, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.url
 
 class Video(models.Model):
-    url = models.CharField(max_length=30, null=True)
+    url = models.CharField(max_length=100, null=True)
     band = models.ForeignKey(Band, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.url
 
 class Artist(models.Model):
-    name = models.CharField(max_length=30, null=True)
-    city = models.CharField(max_length=30, null=True)
+    name = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=100, null=True)
     bands = models.ManyToManyField(Band)
 
     def __str__(self):
         return self.name
 
 class ArtistInstrument(models.Model):
-    type = models.CharField(max_length=30, null=True)
+    type = models.CharField(max_length=100, null=True)
     instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE, null=True)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=True)
 
